47602583672eadfe235bf54e5ead83e3f7e50633
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-16 05:09:40 +0100

    feat: add test for a calculator object

--- tests.test.js
+++ tests.test.js
@@ -1,5 +1,6 @@
-const capitalize = require("./src/capitalize");
-const reverseString = require("./src/reverse");
+const capitalize = require('./src/capitalize');
+const reverseString = require('./src/reverse');
+const calculator = require('./src/calculator')
 
 test('capitalizes the first letter', () => {
   expect(capitalize('hello')).toMatch('Hello');
@@ -7,4 +8,11 @@ test('capitalizes the first letter', () => {
 
 test('reverse a string', () => {
   expect(reverseString('hello')).toMatch('olleh');
+})
+
+test('correctly performs a calculation', () => {
+  expect(calculator.add(2, 2).toBe(4));
+  expect(calculator.subtract(2, 2).toBe(0));
+  expect(calculator.divide(2, 2).toBe(1));
+  expect(calculator.multiply(2, 2).toBe(4));
 })
