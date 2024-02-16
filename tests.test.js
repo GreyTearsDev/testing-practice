7105d7719a805891dca7cd248a6018751f20a667
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-16 04:30:08 +0100

    feat: write test for reverseString() function

--- tests.test.js
+++ tests.test.js
@@ -3,3 +3,7 @@ const capitalize = require("./src/capitalize");
 test('capitalizes the first letter', () => {
   expect(capitalize('hello')).toMatch('Hello');
 })
+
+test('reverse a string', () => {
+  expect(reverseString('hello').toMatch('olleh'));
+})
