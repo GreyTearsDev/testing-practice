8ddd66f8268eda43ff8cad63746817200c124ae0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-16 05:20:50 +0100

    fix: fix typos on the test for the calculator object

--- tests.test.js
+++ tests.test.js
@@ -1,6 +1,5 @@
-const capitalize = require('./src/capitalize');
-const reverseString = require('./src/reverse');
-const calculator = require('./src/calculator')
+const capitalize = require("./src/capitalize");
+const reverseString = require("./src/reverse");
 
 test('capitalizes the first letter', () => {
   expect(capitalize('hello')).toMatch('Hello');
@@ -11,8 +10,8 @@ test('reverse a string', () => {
 })
 
 test('correctly performs a calculation', () => {
-  expect(calculator.add(2, 2).toBe(4));
-  expect(calculator.subtract(2, 2).toBe(0));
-  expect(calculator.divide(2, 2).toBe(1));
-  expect(calculator.multiply(2, 2).toBe(4));
+  expect(calculator.add(2, 2)).toBe(4);
+  expect(calculator.subtract(2, 2)).toBe(0);
+  expect(calculator.divide(2, 2)).toBe(1);
+  expect(calculator.multiply(2, 2)).toBe(4);
 })
