a0b37de06b3ed69a71d6811ff014cfbac2345b3e
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-16 19:12:52 +0100

    feat: add tests for analyzeArray() function

--- tests.test.js
+++ tests.test.js
@@ -2,6 +2,7 @@ const capitalize = require('./src/capitalize');
 const reverseString = require('./src/reverse');
 const calculator = require('./src/calculator')
 const caesar = require('./src/caesar');
+const analyzeArray = require('./src/analyse');
 
 test('capitalizes the first letter', () => {
   expect(capitalize('hello')).toMatch('Hello');
@@ -21,4 +22,13 @@ test('correctly performs a calculation', () => {
 test('shifts each character by the shift factor', () => {
   expect(caesar('hello', 3)).toMatch('KHOOR');
   expect(caesar('incapaz', 7)).toMatch('PUJHWHG');
+})
+
+test('object has "average", "min", "max", and "legth" properties', () => {
+  const object = analyzeArray([1,8,3,4,2,6])
+
+  expect(object.average).toBeDefined();
+  expect(object.min).toBeDefined();
+  expect(object.max).toBeDefined();
+  expect(object.lenght).toBeDefined();
 })
