8ac86b031b09ca94570eeca39c1d028dd77f5f41
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-02-16 04:26:10 +0100

    fix: switch import statement for the require() method
    now it correctly imports the modules
    also deleted duplicated test file

--- src/tests.jest.js
+++
@@ -1,5 +0,0 @@
-import capitalize from "./src/capitalize";
-
-test('capitalizes the first letter', () => {
-  expect(capitalize('hello')).toMatch('Hello');
-})
--- tests.test.js
+++ tests.test.js
@@ -1,4 +1,4 @@
-import capitalize from "./capitalize";
+const capitalize = require("./src/capitalize");
 
 test('capitalizes the first letter', () => {
   expect(capitalize('hello')).toMatch('Hello');
